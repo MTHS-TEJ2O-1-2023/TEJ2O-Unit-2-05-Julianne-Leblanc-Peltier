61a96791e4adc267dce92935e36d2bf1b97161e5
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-09-26 13:07:15 -0400

    PLS IM ALMOST DONE

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@
  * Created on: Sep 2023
  * This program displays the current temperature on a MicroBit
 */
-//variable for random number
+//variable for random number 
 
 let temperature: number
 let temperatureKelvin: number
@@ -13,13 +13,13 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-  //get the temperature in Kelvin
+  //get the temperature in Kelvin 
 
   temperature = input.temperature()
   temperatureKelvin = (temperature + 273.15)
   temperatureKelvin = Math.round(temperatureKelvin)
 
-  //output answer
+  //output answer 
 
   basic.showString('The temperature is: ' + temperatureKelvin.toString() + 'K.')
 })
