0d323861aac3cb6b28e87029257481e3775e5d24
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-09-26 12:20:14 -0400

    edited

--- main.ts
+++ main.ts
@@ -5,12 +5,13 @@
  * This program displays the current temperature on a MicroBit
 */
 
+let temperature = input.temperature()
+let temperatureKelvin = (temperature + 273)
+
 basic.clearScreen()
 basic.pause(1000)
 
 input.onButtonPressed(Button.A, function () {
-    let temperature = input.temperature()
-    let temperatureKelvin = (temperature + 273)
-    basic.showString("The temperature is" + temperatureKelvin)
-    basic.showString("K.")
+  basic.showString("The temperature is" + temperatureKelvin)
+  basic.showString("K.")
 })
