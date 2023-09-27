0042f825df7d20fb9cbc4d714ef68434ffeeda01
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-09-27 01:20:58 +0000

    python version!! :P

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,17 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Julianne Leblanc-Peltier
+Created on: Sep 2023
 This module is a Micro:bit MicroPython program
+This program outputs the temperature in Kelvin on a Micro:Bit
 """
 
 from microbit import *
 
+display.clear()
+display.show(Image.HAPPY)
 
-display.scroll("Hello, World!")
+while True:
+    if button_a.is_pressed():
+        temperature = input.temperature()
+        temperature_kelvin = temperature + 273.15
+        display.show(str(temperature_kelvin))
